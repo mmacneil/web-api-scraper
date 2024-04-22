2ebbc0db0a4a37baef4b9ae84a8c00fe0ab02153
mmacneil <markmacneil@gmail.com>
2024-04-21 22:05:05 -0300

    post wip

--- Program.cs
+++ Program.cs
@@ -1,20 +1,40 @@
+using Hangfire;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Hangfire services.
+builder.Services.AddHangfire(configuration =>
+    configuration
+        .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseSqlServerStorage(builder.Configuration.GetConnectionString("AppConnection"))
+);
+
+// Add the processing server as IHostedService
+builder.Services.AddHangfireServer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHangfireDashboard(
+    "/hangfire",
+    new DashboardOptions() { DashboardTitle = "Hangfire Dashboard" }
+);
 
 app.UseHttpsRedirection();
 
