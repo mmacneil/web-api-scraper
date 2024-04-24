b66e864d63046f988441a0bd5601c4996a483b41
mmacneil <markmacneil@gmail.com>
2024-04-23 22:03:40 -0300

    add table init step

--- Infrastructure/DataContext.cs
+++ Infrastructure/DataContext.cs
@@ -14,6 +14,7 @@ public class DataContext(IConfiguration configuration)
     public async Task Init()
     {
         await InitDatabase();
+        await InitTables();
     }
 
     private async Task InitDatabase()
@@ -25,6 +26,29 @@ public class DataContext(IConfiguration configuration)
 
         await connection.ExecuteAsync(
             "IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'WebApiScraper') CREATE DATABASE [WebApiScraper];"
+        );
+    }
+
+    private async Task InitTables()
+    {
+        // create tables if they don't exist
+        using var connection = CreateDbConnection(
+            configuration.GetConnectionString("AppDb") ?? throw new InvalidOperationException()
+        );
+        await connection.ExecuteAsync(
+            """
+            IF OBJECT_ID('SeasonStatistics', 'U') IS NULL
+            CREATE TABLE SeasonStatistics (
+                SeasonStatisticId INT NOT NULL PRIMARY KEY IDENTITY,
+                 PlayerId INT, Season NVARCHAR(MAX), Team NVARCHAR(MAX), League NVARCHAR(MAX), GamesPlayed INT, Goals INT, Assists INT, Points INT, PenaltyMinutes INT, PlusMinus INT NULL, CreatedAtUtc DATETIME DEFAULT GETUTCDATE());
+
+            IF OBJECT_ID('Players', 'U') IS NULL
+            CREATE TABLE Players (
+                PlayerId INT NOT NULL PRIMARY KEY IDENTITY,
+                Name NVARCHAR(MAX),
+                HomeTown NVARCHAR(MAX),	CreatedAtUtc DATETIME DEFAULT GETUTCDATE()
+            );
+            """
         );
     }
 }
